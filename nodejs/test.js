9f58573d609af0e24dfc747c9fb62ac3c7df3a95
Shi Yan <billconan@gmail.com>
2021-12-26 10:27:15 -0800

    add unit test boilerplate for nodejs binding
    fixes #4

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,11 +1,31 @@
 const Database = require('./index')
+const fs = require('fs')
 
-console.log(Database)
 
-let db = new Database("test.db")
+describe('Hoardbase', () => {
+    let db = null
+    let path = 'test.db'
+    beforeAll(() => {
+        if (fs.existsSync(path)) {
+            fs.unlinkSync(path)
+        }
+        db = new Database("test.db")
+    });
+    afterAll(() => {
+        if (fs.existsSync(path)) {
+            fs.unlinkSync(path)
+        }
+    });
+    beforeEach(() => {
 
-let col = db.createCollection("test")
+    });
+    afterEach(() => {
 
-let r = col.insertOne({data: "test", age: 23, test_arr: [1, 2, 3], test_obj: {a: 1, b: 2}})
+    });
+    test('test insert_one', () => {
+        let col = db.createCollection("test")
+        let r = col.insertOne({ data: "test", age: 23, test_arr: [1, 2, 3], test_obj: { a: 1, b: 2 } })
+        expect(r._id).toBe(1);
+    });
+});
 
-console.log(r)
